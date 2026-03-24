bf78a4680b9fa1625f1f12c241f57549bb7fdb84
Rupesh Kumar SIngh <rupesh.kumar.singh.0068@gmail.com>
2026-03-25 02:13:23 +0530

    Add routes for Twitter and Login pages

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
+app.get("/twitter", (req, res) => {
+  res.send("<h1>Please see the ui of Twitter</h1>");
+});
+app.get("/login", (req, res) => {
+  res.send("Login page");
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening at ${port}`);
